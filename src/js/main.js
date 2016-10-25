27e39de186ec62aca607060acd55770035a34711
AndrusovaK <andrusova_ksenia@mail.ru>
2016-10-25 17:39:04 +0300

    projects slider

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,21 @@
 $(document).ready(function() {
+
+	//Initialization of site scroll
 	$('#fullpage').fullpage({
 		anchors: ['top', 'products', '', 'projects', 'rok', 'services', 'rocks', '', 'contacts'],
 		menu: '#nav',
 		scrollOverflow:true
 	});
+
+	//Slider initialization
+
+	$('.projects__slider').slick({
+		prevArrow: $('.prev'),
+		nextArrow: $('.next'),
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		infinite: false
+	});
+
+
 });
