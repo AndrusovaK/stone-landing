3fd98801bfe4a762f10e83dd3557c02ac9c1cf6e
AndrusovaK <andrusova_ksenia@mail.ru>
2016-10-26 21:04:41 +0300

    map

--- src/js/main.js
+++ src/js/main.js
@@ -37,8 +37,35 @@ $(document).ready(function() {
 		mainClass: 'mfp-fade',
 		removalDelay: 300
 	});
-
-
-
-
 });
+
+ymaps.ready(init);
+
+function init() {
+	var map = new ymaps.Map('map', {
+		center: [59.900946, 30.353367],
+		zoom: 17,
+		controls: []
+	});
+
+	var balloonLayout = ymaps.templateLayoutFactory.createClass(
+			'<div class="map__balloon-wrapper">' +
+			'<div class="map__balloon">' +
+			'<span class="map__balloon-title">Головной офис</span>' +
+			'<p class="map__balloon-address">г.Санкт-Петербург, <br>' +
+			'Камчатская улица, д. 13А</p>' +
+			'</div>' +
+			'<div class="map__balloon-triangle"></div>' +
+			'</div>'
+	);
+
+	var placemark = new ymaps.Placemark([59.900556, 30.350434], {}, {
+		hideIconOnBalloonOpen: true,
+		balloonLayout: balloonLayout,
+		balloonOffset: [-40, -140]
+	});
+
+	map.geoObjects.add(placemark);
+	map.behaviors.disable('scrollZoom');
+	placemark.balloon.open();
+}
